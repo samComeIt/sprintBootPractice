4520596acb8eaa5dea2076ecc89d1fdec53ad416
김산해 <kimsanhae@gimsanhaes-MacBook-Air.local>
2023-06-22 21:25:44 +0900

    Building REST API to update(PUT)

--- src/main/java/com/example/learnspringboot/courses/controller/CourseController.java
+++ src/main/java/com/example/learnspringboot/courses/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,7 +46,10 @@ public class CourseController {
 		repository.save(course);
 	}
 	
-	
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		repository.save(course);
+	}
 		
 		
 }
