297f52856a8f9dec40a68fa010ac3969b38999c1
김산해 <kimsanhae@gimsanhaes-MacBook-Air.local>
2023-06-22 21:31:45 +0900

    Building REST API to delete a course (DELETE)

--- src/main/java/com/example/learnspringboot/courses/controller/CourseController.java
+++ src/main/java/com/example/learnspringboot/courses/controller/CourseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,11 @@ public class CourseController {
 	@PutMapping("/courses/{id}")
 	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
 		repository.save(course);
+	}
+	
+	@DeleteMapping("/course/{id}")
+	public void deleteCourse(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 		
 		
